7441551af13580bd58be9962d26966cb9ee093a5
meghpatil21 <megh.after18@gmail.com>
2022-10-21 23:06:51 +0530

    Implemented Uc1

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -5,6 +5,22 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Computation Program");
+            Console.WriteLine();
+
+            Console.WriteLine("Enter X Co-ordinates of first point: ");
+            Double x1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Y Co-ordinates of first point: ");
+            Double y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter X Co-ordinates of second point: ");
+            Double x2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter X Co-ordinates of second point: ");
+            Double y2 = Convert.ToDouble(Console.ReadLine());
+
+            double x = (x2 - x1) * (x2 - x1);
+            double y = (y2 - y1) * (y2 - y1);
+
+            double lengthOfLine = Math.Sqrt(x + y);
+            Console.WriteLine("Lengh of line is " + lengthOfLine);
         }
     }
 }
